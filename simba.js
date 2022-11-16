829b4d0319416fa64778d7039c316cc808a382f7
ranbarry <114967826+ranbarry@users.noreply.github.com>
2022-11-15 20:19:33 -0500

    Update simba.js

--- simba.js
+++ simba.js
@@ -2,6 +2,32 @@ function play(){
 
 }
 
-function theme(){
-    
+function theme1(){
+
+    document.querySelector("body").style.backgroundImage="url('https://img.freepik.com/free-vector/gradient-christmas-tinsel-background_52683-76117.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition="absolute";
+    document.body.style.backgroundSize="100%";
+
+}
+
+function theme2(){
+    document.getElementById("body").style.backgroundImage="url('https://www.yorku.ca/edu/wp-content/uploads/sites/28/2020/11/yorku-west-entrance-stone.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition="absolute";
+    document.body.style.backgroundSize="100%"; 
+}
+
+function theme3(){
+    document.getElementById("body").style.backgroundImage="url('https://static.toiimg.com/photo/msid-87392351/87392351.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition="absolute";
+    document.body.style.backgroundSize="100%"; 
+}
+
+function theme4(){
+    document.getElementById("body").style.backgroundImage="url('https://media.cntraveler.com/photos/5eb18e42fc043ed5d9779733/master/pass/BlackForest-Germany-GettyImages-147180370.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition="absolute";
+    document.body.style.backgroundSize="100%"; 
 }
